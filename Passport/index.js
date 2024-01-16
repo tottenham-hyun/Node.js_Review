0c049bfdea9840a9cdc2cc4526da306b27b566fd
tottenham-hyun <hwk3988@gmail.com>
2024-01-17 01:30:56 +0900

    로그아웃 버튼

--- Passport/index.js
+++ Passport/index.js
@@ -84,6 +84,12 @@ app.post('/login',(req,res,next)=>{
     })(req,res,next)
 }) 
 
+app.post('/logout',(req,res,next)=>{
+    req.logOut((err)=>{
+        if(err) return next(err)
+        res.redirect('/login')
+    })
+})
 
 app.get('/',checkAuth,(req,res)=>{
     res.render('index')
